6f48c3faf6f671cf3421b50221ec2f51273d6bbb
RIOU Kevin <riou.kkevin@gmail.com>
2021-01-26 23:18:20 +0100

    ✨feat (HTTP): Add http route for hosting

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ import Bot from '@discord-ts-app/core/build/Bot'
 import Env from '@discord-ts-app/core/build/Utils/Env'
 import { Week, Help, Hour } from 'App/Commands'
 import { Ready, Message } from 'App/Events'
+import Koa from 'koa'
+
+const app = new Koa();
+
+app.use(async ctx => {
+    ctx.body = 'AmongUs Bot';
+});
+
+app.listen(8000);
 
 Bot.registerCommands([Week, Help, Hour])
     .registerEvents([Ready, Message])
